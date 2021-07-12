334df67ad633e138a59fb85a8e94f3cde4aad6f8
tpw123789 <tpw123789@gmail.com>
2021-07-13 04:13:17 +0800

    modify sparse matric linked list

--- LinkedList/mListedLink.c
+++ LinkedList/mListedLink.c
@@ -1,2 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
+
+typedef struct MListNode{
+	int row, col;
+	float coaf;
+	struct MListNode *right, *down;
+}MListNode;
+
+
+
+int main(void){
+	
+	return 0;
+}
+
+
+
